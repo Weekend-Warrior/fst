2c6a828552dd16796d4cef2f515b3072fa59fa2f
StewartBobbitt <stewbobb@gmail.com>
2018-04-26 16:45:45 -0500

    Fix write.

--- R/sparseMatrix.R
+++ R/sparseMatrix.R
@@ -2,12 +2,16 @@
 write_fst.sparseMatrix <- function(x, path, compress = 50, uniform_encoding = TRUE) {
   if (!is.character(path)) stop("Please specify a correct path.")
 
+  x <- as(x, 'TsparseMatrix')
   classes <- is(x)
 
-  if (any(classes %in% 'dsparseMatrix')) indices <- c('i', 'p', 'x')
-  else indices <- c('i', 'p')
+  if (any(classes %in% 'dsparseMatrix')) indices <- c('i', 'j', 'x')
+  else indices <- c('i', 'j')
 
-  dt <- fststore(normalizePath(path, mustWork = FALSE), as.data.frame(attributes(x)[indices]), as.integer(compress), uniform_encoding)
+  dt <- fststore(normalizePath(path, mustWork = FALSE),
+                 as.data.frame(attributes(x)[indices]),
+                 as.integer(compress),
+                 uniform_encoding)
 
   if (inherits(dt, "fst_error")) {
     stop(dt)
