b42a97a417b529a873d8c55abb9d1e85f5f9d641
Bobbitt <bobbits1@hmcorp.local>
2018-04-27 13:03:03 -0500

    Use primitives.

--- R/sparseMatrix.R
+++ R/sparseMatrix.R
@@ -2,14 +2,13 @@
 write_fst.sparseMatrix <- function(x, path, compress = 50, uniform_encoding = TRUE) {
   if (!is.character(path)) stop("Please specify a correct path.")
 
-  x <- as(x, 'TsparseMatrix')
-  classes <- is(x)
+  x <- attributes(as(x, 'TsparseMatrix'))
 
-  if (any(classes %in% 'dsparseMatrix')) indices <- c('i', 'j', 'x')
+  if (any(names(x) %in% 'x')) indices <- c('i', 'j', 'x')
   else indices <- c('i', 'j')
 
   dt <- fststore(normalizePath(path, mustWork = FALSE),
-                 as.data.frame(attributes(x)[indices]),
+                 'attr<-'('attr<-'(x[indices], 'row.names', 1:length(x$i)), 'class', 'data.frame'),
                  as.integer(compress),
                  uniform_encoding)
 
